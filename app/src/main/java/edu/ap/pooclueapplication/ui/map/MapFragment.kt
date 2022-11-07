3debf9b322e772add52ca33ba52d008a21f3a348
kenx00x <kenx00x@gmail.com>
2022-11-08 00:52:57 +0100

    add map

--- app/src/main/java/edu/ap/pooclueapplication/ui/map/MapFragment.kt
+++ app/src/main/java/edu/ap/pooclueapplication/ui/map/MapFragment.kt
@@ -1,13 +1,20 @@
 package edu.ap.pooclueapplication.ui.map
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import edu.ap.pooclueapplication.databinding.FragmentMapBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+
 
 class MapFragment : Fragment() {
 
@@ -17,24 +24,50 @@ class MapFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val map = binding.mapview
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), map)
+                locationOverlay.enableMyLocation()
+                locationOverlay.enableFollowLocation()
+                map.overlays.add(locationOverlay)
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), map)
+                locationOverlay.enableMyLocation()
+                locationOverlay.enableFollowLocation()
+                map.overlays.add(locationOverlay)
+            } else -> {
+            // No location access granted.
+        }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val mapViewModel =
-            ViewModelProvider(this).get(MapViewModel::class.java)
+        Configuration.getInstance().setUserAgentValue("user")
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        mapViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val map = binding.mapview
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true);
+        map.setClickable(true);
+        map.setUseDataConnection(true);
+        map.controller.setZoom(18.0)
+        map.minZoomLevel = 10.0
         return root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
