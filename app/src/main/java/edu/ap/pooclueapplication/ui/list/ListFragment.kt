a8bbde1806a33161c4c762c3c3b1941aef586bcd
kenx00x <kenx00x@gmail.com>
2022-11-09 21:28:46 +0100

    rename notifications to list

--- app/src/main/java/edu/ap/pooclueapplication/ui/list/ListFragment.kt
+++ app/src/main/java/edu/ap/pooclueapplication/ui/list/ListFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import edu.ap.pooclueapplication.databinding.FragmentNotificationsBinding
+import edu.ap.pooclueapplication.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class ListFragment : Fragment() {
         val listViewModel =
             ViewModelProvider(this).get(ListViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
