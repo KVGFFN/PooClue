b94196f61a94cfc3ff3c6670865b5ae725f32fe9
kenx00x <kenx00x@gmail.com>
2022-11-09 21:58:45 +0100

    filter toilet type

--- app/src/main/java/edu/ap/pooclueapplication/ui/filter/FilterFragment.kt
+++ app/src/main/java/edu/ap/pooclueapplication/ui/filter/FilterFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import edu.ap.pooclueapplication.databinding.FragmentFilterBinding
 
 class FilterFragment : Fragment() {
@@ -22,17 +20,8 @@ class FilterFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val filterViewModel =
-            ViewModelProvider(this).get(FilterViewModel::class.java)
-
         _binding = FragmentFilterBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        filterViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
