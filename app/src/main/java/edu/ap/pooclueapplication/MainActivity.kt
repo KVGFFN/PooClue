be3a2852d3b094b251d7d32a2cd783b746f01c55
kenx00x <kenx00x@gmail.com>
2022-11-09 13:07:28 +0100

    optimize imports

--- app/src/main/java/edu/ap/pooclueapplication/MainActivity.kt
+++ app/src/main/java/edu/ap/pooclueapplication/MainActivity.kt
@@ -1,12 +1,12 @@
 package edu.ap.pooclueapplication
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.ap.pooclueapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
