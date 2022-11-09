cccdbf51e62761733fe0bff13698f2ba6c2361dd
kenx00x <kenx00x@gmail.com>
2022-11-09 21:30:27 +0100

    rename home to filter

--- app/src/main/java/edu/ap/pooclueapplication/ui/filter/FilterFragment.kt
+++ app/src/main/java/edu/ap/pooclueapplication/ui/filter/FilterFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import edu.ap.pooclueapplication.databinding.FragmentHomeBinding
+import edu.ap.pooclueapplication.databinding.FragmentFilterBinding
 
 class FilterFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentFilterBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class FilterFragment : Fragment() {
         val filterViewModel =
             ViewModelProvider(this).get(FilterViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentFilterBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
